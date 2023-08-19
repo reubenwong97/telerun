780a713b83c71087e8c1da24dd79540e81c353f7
Reuben Wong <reuben.wong.ys@gmail.com>
2023-08-19 16:45:57 +0800

    correct sql code

--- migrations/20230818165230_schema.sql
+++ migrations/20230818165230_schema.sql
@@ -1,21 +1,13 @@
 -- Add migration script here
+CREATE TABLE IF NOT EXISTS chats (id bigserial PRIMARY KEY);
 CREATE TABLE IF NOT EXISTS users (
     id serial PRIMARY KEY,
+    chat_id bigserial REFERENCES chats(id),
     user_name varchar(32) NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS chats (
-    id bigserial PRIMARY KEY,
-);
-
 CREATE TABLE IF NOT EXISTS runs (
     id serial PRIMARY KEY,
     distance real NOT NULL,
     medals integer NOT NULL,
-    constraint user_id
-        foreign key(id)
-            references users(id)
-    constraint chat_id
-        foreign key(id)
-            references chats(id)
+    user_id serial REFERENCES users(id)
 );
