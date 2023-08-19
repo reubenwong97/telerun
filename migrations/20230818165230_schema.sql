9eee69cc855a23b072ad01c05bbe6ea29174601e
Reuben Wong <reuben.wong.ys@gmail.com>
2023-08-19 15:48:59 +0800

    skeleton bot code

--- migrations/20230818165230_schema.sql
+++ migrations/20230818165230_schema.sql
@@ -1,7 +1,21 @@
 -- Add migration script here
+CREATE TABLE IF NOT EXISTS users (
+    id serial PRIMARY KEY,
+    user_name varchar(32) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS chats (
+    id bigserial PRIMARY KEY,
+);
+
 CREATE TABLE IF NOT EXISTS runs (
     id serial PRIMARY KEY,
     distance real NOT NULL,
     medals integer NOT NULL,
-    user_id varchar(32) NOT NULL
+    constraint user_id
+        foreign key(id)
+            references users(id)
+    constraint chat_id
+        foreign key(id)
+            references chats(id)
 );
--- src/bot.rs
+++ src/bot.rs
@@ -1,5 +1,5 @@
 use sqlx::PgPool;
-use teloxide::{macros::BotCommands, prelude::*};
+use teloxide::{prelude::*, utils::command::BotCommands};
 
 pub struct BotService {
     pub bot: Bot,
@@ -10,6 +10,10 @@ pub struct BotService {
 impl shuttle_runtime::Service for BotService {
     async fn bind(self, _addr: std::net::SocketAddr) -> Result<(), shuttle_runtime::Error> {
         // Start your service and bind to the socket address
+        self.start()
+            .await
+            .expect("An error occured while using the bot!");
+
         Ok(())
     }
 }
@@ -19,9 +23,57 @@ impl BotService {
         let bot = self.bot.clone();
         let db_connection = self.postgres.clone();
 
+        Command::repl(bot, move |bot, msg, cmd| {
+            answer(bot, msg, cmd, db_connection.clone())
+        })
+        .await;
+
         Ok(())
     }
 }
 
 #[derive(BotCommands, Clone)]
-enum Command {}
+#[command(
+    rename_rule = "lowercase",
+    description = "The following commands are supported:"
+)]
+enum Command {
+    #[command(description = "display this text")]
+    Help,
+    #[command(
+        description = "Add run data to database. Format is /add %distance (km)% %username%",
+        parse_with = "split"
+    )]
+    Add { distance: f32, user_name: String },
+    #[command(description = "Edit data for a run.", parse_with = "split")]
+    Edit { run_id: u32, distance: f32 },
+    #[command(description = "Remove a run from database.")]
+    Delete { run_id: u32 },
+    #[command(description = "Tallies current medals and distances.")]
+    Tally,
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command, db_connection: PgPool) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => {
+            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                .await?;
+        }
+        Command::Add {
+            distance,
+            user_name,
+        } => {
+            bot.send_message(msg.chat.id, "Add".to_string()).await?;
+        }
+        Command::Edit { run_id, distance } => {
+            bot.send_message(msg.chat.id, "Edit".to_string()).await?;
+        }
+        Command::Delete { run_id } => {
+            bot.send_message(msg.chat.id, "Delete".to_string()).await?;
+        }
+        Command::Tally => {
+            bot.send_message(msg.chat.id, "Tally".to_string()).await?;
+        }
+    }
+    Ok(())
+}
